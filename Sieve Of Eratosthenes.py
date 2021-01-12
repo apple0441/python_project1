75268d12045f3058d135f668bb84522ffbabf2b7
apple0441 <simuliu711@gmail.com>
2021-01-12 13:48:20 +0530

    Sieve Of Eratosthenes.

--- Sieve Of Eratosthenes.py
+++ Sieve Of Eratosthenes.py
@@ -1,14 +1,15 @@
 def SieveOfEratosthenes(n):
     p = 2
     primes = []
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         primes.append(i)
-    while(p * p <= n):
-        for i in range(p*p, n+1, p):
+    while (p * p <= n):
+        for i in range(p * p, n + 1, p):
             if i in primes:
                 primes.remove(i)
-        p+=1
+        p += 1
     return primes
+
 
 if __name__ == '__main__':
     print(SieveOfEratosthenes(30))
